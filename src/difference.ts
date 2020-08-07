8fb3d295861e633e7d0751f2bc6f58ded6ee7e9b
KylinLee <kylincottage@163.com>
2020-08-07 23:03:46 +0800

    bug fixed

--- src/difference.ts
+++ src/difference.ts
@@ -1,14 +1,19 @@
-export default function (array: any[], value: any[] = []) {
-    if (value instanceof Array && value.length !== 0) {
-        const filter = new Set(value);
-        const newArray: any = [];
-        for (const item of array) {
-            if (!filter.has(item)) {
-                newArray.push(item)
+export default function (array: any[] | any, value: any[] = []) {
+    if (array instanceof Array) {
+        if (value instanceof Array && value.length !== 0) {
+            const filter = new Set(value);
+            const newArray: any = [];
+            for (const item of array) {
+                if (!filter.has(item)) {
+                    newArray.push(item)
+                }
             }
+            return newArray;
+        } else {
+            return new Array(...array);
         }
-        return newArray;
     } else {
-        return new Array(...array);
+        return []
     }
+
 }
