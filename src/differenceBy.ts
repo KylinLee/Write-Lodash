d80794bda13297ae936b7cc2287001a1859f00ae
KylinLee <kylincottage@163.com>
2020-08-08 22:15:35 +0800

    完善differenceBy的兼容性

--- src/differenceBy.ts
+++ src/differenceBy.ts
@@ -3,19 +3,41 @@ import identity from "./identity"
 export default function (array: any[] | any, value: any[] | any = [], iteratee: any | any[] = identity) {
     if (array instanceof Array) {
         if (value instanceof Array && value.length !== 0) {
+            let iterator: (item: any | any[]) => any
             if (typeof iteratee === "function") {
-                iteratee = iteratee
+                iterator = iteratee
             } else if (typeof iteratee === "string") {
-                const key = iteratee;
-                iteratee = (item: any | any[]) => {
+                iterator = (item) => {
                     if (typeof item === "object") {
-                        return item[key]
+                        return item[iteratee]
                     }
                 }
+            } else if (iteratee instanceof Array) {
+                iterator = (item) => {
+                    if (iteratee.indexOf(item) === -1) {
+                        return item
+                    }
+                }
+            } else if (iteratee instanceof Object) {
+                const keys = Object.keys(iteratee)
+                iterator = (item) => {
+                    if (item instanceof Object) {
+                        for (const val of keys) {
+                            if (item.hasOwnProperty(val) && JSON.stringify(item[val]) === JSON.stringify(iteratee[val])) {
+                                return JSON.stringify(item)
+                            }
+                        }
+                    } /* else {
+                        // 当传入的iteratee为对象时，lodash的原始数组和过滤数组的每一项必须为对象
+                        // 如[{x:0},1]这种情况下，lodash会返回空数组[]
+                        // 这样似乎不够强大，lodash在这种特殊情况下无法使用，取消此注释，将兼容这种情况
+                        return item
+                    } */
+                }
             }
-            value = value.map((item) => { return iteratee(item) })
+            value = value.map((item) => { return iterator(item) })
             const filter = new Set(value);
-            const transArray = array.map((item) => { return iteratee(item) })
+            const transArray = array.map((item) => { return iterator(item) })
             const newArray: any = [];
             transArray.forEach((val, index) => {
                 if (!filter.has(val)) {
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,15 @@ import _ from "lodash";
 const origin = [false, null, 0, NaN, "", "string", undefined, 1, 2, 3, {}, [], [1]];
 const filter = [false, null, 0, NaN, "", "string", undefined, 1, 2, 3, {}, [], [1], 1, 2, 3];
 
+// 第三个参数接收函数
 console.log("lodash", _.differenceBy([3.1, 2.2, 1.3], [4.4, 2.5], Math.floor));
 console.log("自己实现", dash.differenceBy([3.1, 2.2, 1.3], [4.4, 2.5], Math.floor));
+// 第三个参数接收字符串
 console.log("lodash", _.differenceBy([{ 'x': 2 }, { 'x': 1 }], [{ 'x': 1 }], "x"));
 console.log("自己实现", dash.differenceBy([{ 'x': 2 }, { 'x': 1 }], [{ 'x': 1 }], "x"));
+// 第三个参数接收数组
+console.log("lodash", _.differenceBy([1, 2, 3, 4, 5], [1, 2], [2, 3]));
+console.log("自己实现", dash.differenceBy([1, 2, 3, 4, 5], [1, 2], [2, 3]));
+// 第三个参数接收对象
+console.log("lodash", _.differenceBy([{ 'x': 2 }, { 'x': 1 }, 3], [3, { 'x': 1 }], { x: 1 }))
+console.log("自己实现", dash.differenceBy([{ 'x': 2 }, { 'x': 1 }, 3], [3, { 'x': 1 }], { x: 1 }))
